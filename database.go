0df3507e9b43f5ceb2271e3a9a4d11790455ae44
sahanyarar <yararsahan@gmail.com>
2022-11-04 15:23:17 +0300

    Main_push

--- database.go
+++ database.go
@@ -8,7 +8,7 @@ import (
 )
 
 func con_database() *gorm.DB {
-	db, err := gorm.Open("postgres", "host=localhost port=8085 user=your_user dbname=deneme_01 password=toor")
+	db, err := gorm.Open("postgres", "host=localhost port=8085 user=sahan dbname=deneme_01 password=toor")
 
 	if err != nil {
 		fmt.Println(err)
--- main.go
+++ main.go
@@ -10,23 +10,13 @@ func main() {
 
 	User_Hand := New_Hand(User_Repo)
 
-	route := gin.Default()
-	route.GET("/user/crate", User_Hand.New_User)
+	r := gin.Default()
+	r.POST("/user/crate", User_Hand.New_User)
+	r.GET("/test", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ok!!!",
+		})
+	})
 
-	route.Run(":8085")
+	r.Run(":8085")
 }
-
-/*
-func startPage(c *gin.Context) {
-	var person Person
-	// If `GET`, only `Form` binding engine (`query`) used.
-	// If `POST`, first checks the `content-type` for `JSON` or `XML`, then uses `Form` (`form-data`).
-	// See more at https://github.com/gin-gonic/gin/blob/master/binding/binding.go#L48
-	if c.ShouldBind(&person) == nil {
-		log.Println(person.Name)
-		log.Println(person.Address)
-		log.Println(person.Birthday)
-	}
-
-	c.String(200, "Success")
-}*/
